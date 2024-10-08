dcbe21ed2e188f7d78f9e616179acd154efcd471
Brian Valente <brian@valente.com.ar>
2024-10-08 06:28:54 -0700

    Match logic (#19)
    **Descripción**
    
    Este PR implementa la lógica principal del juego. Una clase Match que
    permite agregar barcos y realizar disparos.
    
    Deberá conectarse con la UI.

---
+++ tests/test_match.py
@@ -0,0 +1,45 @@
+import json
+from uade_battleship.match import Match, MatchCellState
+
+
+def test_new_match():
+    match = Match("Player 1", "Player 2")
+    players = match.get_players()
+    assert players == ("Player 1", "Player 2")
+    assert match.get_board(1)[0][0] == MatchCellState.EMPTY
+
+    match.add_ship(1, {"x": 0, "y": 0, "size": 3, "orientation": "horizontal"})
+    match.add_ship(2, {"x": 0, "y": 0, "size": 3, "orientation": "vertical"})
+    assert match.get_board(1)[0][0] == MatchCellState.SHIP
+
+    match.add_shot(1, {"x": 0, "y": 0})
+    match.add_shot(2, {"x": 0, "y": 0})
+    assert match.get_board(1)[0][0] == MatchCellState.HIT
+
+
+def test_loaded_match():
+    match_1 = Match("Player 1", "Player 2")
+    match_1.add_ship(1, {"x": 0, "y": 0, "size": 3, "orientation": "horizontal"})
+    match_1.add_shot(1, {"x": 0, "y": 0})
+    json_data = match_1.get_json()
+    match_2 = Match.from_json(json_data)
+    assert match_1.get_players() == match_2.get_players()
+    assert json.dumps(match_1.get_board(1)) == json.dumps(match_2.get_board(1))
+
+
+def test_out_of_bounds_ship():
+    match = Match("Player 1", "Player 2")
+    try:
+        match.add_ship(1, {"x": 8, "y": 0, "size": 3, "orientation": "horizontal"})
+        assert False
+    except ValueError:
+        assert True
+
+
+def test_out_of_bounds_shot():
+    match = Match("Player 1", "Player 2")
+    try:
+        match.add_shot(1, {"x": 10, "y": 0})
+        assert False
+    except ValueError:
+        assert True
---
+++ uade_battleship/match/__init__.py
@@ -0,0 +1,11 @@
+from .match import Match
+from .match_data import MatchCellState, Coord, MatchData, PlayerData, ShipPosition
+
+__all__ = [
+    "Match",
+    "MatchCellState",
+    "Coord",
+    "MatchData",
+    "PlayerData",
+    "ShipPosition",
+]
