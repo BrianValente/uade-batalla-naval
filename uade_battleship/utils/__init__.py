a3dee23dcafe46d7d685926d4622828369a02084
Brian Valente <brian@valente.com.ar>
2024-12-01 22:43:37 -0300

    Delete `suma`

--- uade_battleship/utils/__init__.py
+++ uade_battleship/utils/__init__.py
@@ -1,9 +1,3 @@
 from .file_storage import FileStorage
 from .settings import Settings, SettingsKey
 from .colors import Color
-
-
-def Suma(a: int, b: int) -> int:
-    suma: int = a + b
-
-    return suma
